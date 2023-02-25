3c126f9bf11458906a4655a878d9dabcb088f72a
Ben <jumpeebunee@gmail.com>
2023-02-25 18:58:07 +0300

    feat: add images

--- Scene1.js
+++ Scene1.js
@@ -2,4 +2,23 @@ class Scene1 extends Phaser.Scene {
   constructor() {
     super('bootGame');
   }
+  preload() {
+    this.load.image('background', './assets/background.png');
+
+    this.load.spritesheet('ship1', './assets/ship1.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    })
+    this.load.spritesheet('ship2', './assets/ship2.png', {
+      frameWidth: 32,
+      frameHeight: 16,
+    })
+    this.load.spritesheet('ship3', './assets/ship3.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    })
+  }
+  create() {
+    this.scene.start('playGame');
+  }
 }
--- Scene2.js
+++ Scene2.js
@@ -2,4 +2,12 @@ class Scene2 extends Phaser.Scene {
   constructor() {
     super('playGame');
   }
+  create() {
+    this.background = this.add.image(0, 0, 'background');
+    this.background.setOrigin(0, 0);
+
+    this.ship1 = this.add.sprite(GAME_CONFIG.width / 2 - 50, GAME_CONFIG.height / 2, 'ship1');
+    this.ship2 = this.add.sprite(GAME_CONFIG.width / 2, GAME_CONFIG.height / 2, 'ship2');
+    this.ship3 = this.add.sprite(GAME_CONFIG.width / 2 + 50, GAME_CONFIG.height / 2, 'ship3');
+  }
 }
